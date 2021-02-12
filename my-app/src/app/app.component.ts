0be5bae3b8389e6a1ad560935fb137bfc5e6a087
Igor14545216 <igor_ea@yahoo.com.br>
2021-02-12 10:47:06 -0300

    Add escopo do menu

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.scriptMenu();
+  }
+
+  scriptMenu() {
+    $(document).ready(function () {
+      $('[data-toggle=offcanvas]').click(function () {
+        $('.row-offcanvas').toggleClass('active');
+      });
+    });
+  }
+
 }

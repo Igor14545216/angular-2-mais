453af0a2da6d8e7cf417dc4477ba7d92690f150b
IgorSilva89 <igor.silva@sinqia.com.br>
2021-02-15 14:17:58 -0300

    Componentizacao

--- my-app/src/app/helpers/data-binding/data-binding.component.ts
+++ my-app/src/app/helpers/data-binding/data-binding.component.ts
@@ -5,10 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './data-binding.component.html',
 })
 export class DataBindingComponent implements OnInit {
-
-  url: string = 'https://github.com/Igor14545216/';
+  
+  url: string = 'https://github.com/IgorSilva89/';
   urlImagem: string = 'http://lorempixel.com/400/200/nature/';
   cursoAngular: boolean = true;
+  
+  textoInterpolacao: string = 'A Interpolação nada mais é do que um recurso de incorporar expressões dentro de uma área delimitada por {{ .. }}. ... Além da interpolação, podemos utilizar expressions em tags delimitados por colchetes, capturando o que queremos, seja de uma propriedade, ou de uma referência de variável.'
+
 
   getValor() {
     return 1;

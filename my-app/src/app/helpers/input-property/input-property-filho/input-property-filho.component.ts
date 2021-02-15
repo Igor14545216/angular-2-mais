56a881de64176a6cca2a7cfe68d2c50e23811faf
IgorSilva89 <igor.silva@sinqia.com.br>
2021-02-15 14:31:33 -0300

    Componentizacao

--- my-app/src/app/helpers/input-property/input-property-filho/input-property-filho.component.ts
+++ my-app/src/app/helpers/input-property/input-property-filho/input-property-filho.component.ts
@@ -10,6 +10,8 @@ export class InputPropertyFilhoComponent implements OnInit {
   @Input() nomeVariavelFilho1: string = '';
   @Input('nome') nomeVariavelFilho2: string = '';
 
+  textoExemplo: string = 'No typescript pai foi criado uma variável do tipo string "nomeVariavelPai" já inicializada com um valor. No html pai essa variável foi passada por meio do input property no selector do filho. No typescript filho foi criado uma variável usando o decorator de @Input com o mesmo nome passado no selector do html pai. No HTML filho foi feito uma interpolação para exibir o resultado da variável na tela.';
+
   constructor() { }
 
   ngOnInit(): void {
--- my-app/src/app/helpers/input-property/input-property.component.ts
+++ my-app/src/app/helpers/input-property/input-property.component.ts
@@ -8,6 +8,7 @@ export class InputPropertyComponent implements OnInit {
 
   nomeVariavelPai: string = 'Variável do tipo string passada do componente InputPropertyComponent para o InputPropertyFilhoComponent usando o decorator @Input().';
 
+  textInputProperty: string = 'O conceito é muito simples, você usará isso quando quiser receber dados de um component pai. Neste exemplo, o InputPropertyComponent será o arquivo pai e o InputPropertyFilhoComponent será o filho.'
   constructor() { }
 
   ngOnInit(): void {

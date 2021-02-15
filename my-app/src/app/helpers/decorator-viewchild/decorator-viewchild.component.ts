920c02a8c16ec6771abd687ca4b0ff0b5b50b9c5
IgorSilva89 <igor_ea@yahoo.com.br>
2021-02-15 16:02:15 -0300

    Componentizacao do viewChild

--- my-app/src/app/helpers/decorator-viewchild/decorator-viewchild.component.ts
+++ my-app/src/app/helpers/decorator-viewchild/decorator-viewchild.component.ts
@@ -9,10 +9,19 @@ export class DecoratorViewchildComponent implements OnInit {
   valorEx1: number = 0;
   valorEx2: number = 0;
 
+  textoViewchild: string = 'O ViewChild torna possível acessar um componente filho e chamar métodos ou acessar variáveis de instância que estão disponíveis para o filho, além de acessar um elemento DOM com variável local no HTML. Os dois exemplos a seguir é de acesso ao DOM.';
+
+  textoExemplo1: string = 'Depois de ter criado a variável local no html com `#campoInput`, no typescript foi criado um decorator @ViewChild com a variável `campoValorInput`, e a mesma foi tipada com HTMLElement, podemos ver todos os eventos que um campo input pode disparar, além do value, etc. Para testar clique em um dos dois botões e veja o resultado no console.';
+
+  textoExemplo2: string = 'Após a tipagem da variável de `HTMLElement` para `ElementRef` para fazer o uso de uma interface, odemos manipular a variável de acordo com sua necessidade. Neste exemplo, mostrei apenas o valor da variável `campoInput2` com `this.campoValorInput2.nativeElement.value`. Veja o resultado no console:'
+
   /**
     O "!" A sintaxe existe para aqueles casos comuns em que você não pode garantir que o valor será definido imediatamente.
    */
   @ViewChild('campoInput') campoValorInput!: HTMLElement;
+
+
+
   @ViewChild('campoInput2') campoValorInput2!: ElementRef;
 
   /**
@@ -21,7 +30,7 @@ export class DecoratorViewchildComponent implements OnInit {
    */
   // @ViewChild('campoInput', {static: true}) campoValorInput!: HTMLElement;
 
-  constructor() { 
+  constructor() {
   }
 
   ngOnInit(): void {

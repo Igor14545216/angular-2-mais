c2676c16b596e6faf396285b6c21027d8b4f7150
IgorSilva89 <igor_ea@yahoo.com.br>
2021-02-15 16:01:42 -0300

    Componentizacao do output property

--- my-app/src/app/helpers/output-property/output-property-filho/output-property-filho.component.ts
+++ my-app/src/app/helpers/output-property/output-property-filho/output-property-filho.component.ts
@@ -9,6 +9,10 @@ export class OutputPropertyFilhoComponent implements OnInit {
   @Input() valor: number = 0;
   @Input() valorOutput: number = 0;
 
+  textoExemplo1: string = 'Aqui ocorre apenas o Input(). No html pai estou passando para o filho um property fixo com [valor]="10". No typescript filho estou recebendo esse parâmetro com @Input() valor: number = 0.';
+
+  textoExemplo2: string = 'Usando o EventEmitter, consigo passar um valor do filho para o pai, por meio do property @Output(). Resultado no console.log colocado em OutputPropertyComponent.';
+
   @Output() mudouValor = new EventEmitter();
 
   constructor() { }
--- my-app/src/app/helpers/output-property/output-property.component.ts
+++ my-app/src/app/helpers/output-property/output-property.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutputPropertyComponent implements OnInit {
 
+  textoOutputProperty: string = 'Property usado para passar conteúdo do filho para o pai. Desde parâmetros simples até objetos complexos. A ordem de emissão de eventos no angular é a seguinte: Pai emite para o filho: Input(). Filho emite para o pai: @Output().';
+  
   constructor() { }
 
   ngOnInit(): void {

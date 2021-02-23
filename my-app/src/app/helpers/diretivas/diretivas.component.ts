12cd13bc134345049a0835eac6336131f9cafa31
IgorSilva89 <igor_ea@yahoo.com.br>
2021-02-23 13:10:30 -0300

    Correcao ortografica

--- my-app/src/app/helpers/diretivas/diretivas.component.ts
+++ my-app/src/app/helpers/diretivas/diretivas.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasComponent implements OnInit {
 
-  textoDiretivas: string = 'diretivas estruturais são responsáveis por geralmente manipular o DOM, adicionando ou removendo elementos. Podemos reconhecê-los através do asterisco (*) que segue com o atributo da diretiva. Por exemplo, o Angular nos provê algumas diretivas estruturais, como: ngFor, ngIf e ngSwitchCase.';
+  textoDiretivas: string = 'Diretivas estruturais são responsáveis por geralmente manipular o DOM, adicionando ou removendo elementos. Podemos reconhecê-los através do asterisco (*) que segue com o atributo da diretiva. Por exemplo, o Angular nos provê algumas diretivas estruturais, como: ngFor, ngIf e ngSwitchCase.';
   
   constructor() { }
 

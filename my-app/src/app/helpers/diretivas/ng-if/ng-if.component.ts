d38b184c50540795f45258ecaee5ad51c9782636
IgorSilva89 <igor_ea@yahoo.com.br>
2021-02-23 13:10:57 -0300

    Desenvolvimento do componente da diretiva *ngIf

--- my-app/src/app/helpers/diretivas/ng-if/ng-if.component.ts
+++ my-app/src/app/helpers/diretivas/ng-if/ng-if.component.ts
@@ -6,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgIfComponent implements OnInit {
 
-  textoNgIf: string = 'usada para incluir ou excluir um elemento da interface do usuário, incluindo os elementos filhos do elemento. A marcação excluída por uma diretiva ngIf não será invisível, apenas não estará no DOM.';
+  textoNgIf: string = 'Usada para incluir ou excluir um elemento da interface do usuário, incluindo os elementos filhos do elemento. A marcação excluída por uma diretiva ngIf não será invisível, apenas não estará no DOM.';
+  arrayExemplo1: string[] = [];
+  arrayExemplo2: string[] = ["Array preenchido"];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  mudarValor() {
+    if (this.arrayExemplo1.length > 0) this.arrayExemplo1.splice(0, 1);
+    else this.arrayExemplo1.push("Array prenchido");
+
+    if (this.arrayExemplo2.length > 0) this.arrayExemplo2.splice(0, 1);
+    else this.arrayExemplo2.push("Array prenchido");
   }
 
 }
